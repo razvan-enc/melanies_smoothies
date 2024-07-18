fadc82198148c8074765d4156e60b157176bcae6
razvan-enc <razvan.cutuliga@encora.com>
2024-07-18 23:10:34 +0300

    Update streamlit_app.py
    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,71 +1,68 @@
+# Import python packages
 import streamlit as st
-from snowflake.snowpark import Session
-from snowflake.snowpark.exceptions import SnowparkSessionException
-
-# Configurare conexiune Snowflake
-connection_parameters = {
-    "account": "KVFEXRW.TC40940",
-    "user": "crazvan6",
-    "password": "Counter6,.",
-    "role": "SYSADMIN",
-    "warehouse": "COMPUTE_WH",
-    "database": "SMOOTHIES",
-    "schema": "PUBLIC"
-}
-
-def create_snowflake_session():
-    try:
-        session = Session.builder.configs(connection_parameters).create()
-        return session
-    except SnowparkSessionException as e:
-        st.error(f"Could not create Snowflake session: {e}")
-        return None
-
-# Creează sesiunea
-session = create_snowflake_session()
-
-# Verifică dacă sesiunea a fost creată cu succes
-if session:
-    st.write("Snowflake session created successfully!")
-    # Poți continua cu logica aplicației tale folosind `session`
-else:
-    st.error("Failed to create Snowflake session. Please check the connection parameters.")
+import requests
+from snowflake.snowpark.context import get_active_session
+import pandas
 
 # Write directly to the app
-st.title(":smoothie: Example Streamlit App :balloon:")
+st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
 st.write(
-    """Replace this example with your own code!
-    **And if you're new to Streamlit,** check
-    out our easy-to-follow guides at
-    [docs.streamlit.io](https://docs.streamlit.io).
+    """
+    choose the fruits you want in your custom smoothie
     """
 )
 
+
 from snowflake.snowpark.functions import col
-session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.write("debug")
+cnx = st.connection("snowflake")
+session = cnx.session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col("search_on"))
+
+# st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df = my_dataframe.to_pandas()
+
 
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients: ',
+    "What are your favorite fruits",
     my_dataframe
+    ,
+    max_selections = 5
 )
 
-if ingredients_list:
-    ingredients_string = ''
-
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-    st.write(ingredients_string)
-    
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-                values ('""" + ingredients_string + """')"""
-    
-    #st.write(my_insert_stmt)
-    time_to_insert = st.button('Submit Order')
-
-    if time_to_insert:
+ingredients_string  = ''
+if ingredients_list:    
+    # st.write("You selected:", ingredients_list)
+    # st.text(ingredients_list)
+    for fruit in ingredients_list: 
+        st.subheader(fruit + " Nutrition Information" )
         
+        ingredients_string += fruit + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit,' is ', search_on, '.')
+        
+        st.write(ingredients_string)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit)
+        fv_df = st.dataframe(data = fruityvice_response.json(),use_container_width = False)
+
+
+
+name_on_order = st.text_input("Your Name")
+# st.write("Your name entered is", name_on_order)
+
+my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """',
+            '"""+name_on_order+"""')"""
+
+# st.write(my_insert_stmt)
+
+time_to_insert = st.button("Submit Order")
+if time_to_insert: 
+    if ingredients_string:
         session.sql(my_insert_stmt).collect()
-        st.success('Your smoothie is ordered!', icon="✅")
+    st.success(f'Your Smoothie is ordered {name_on_order}!', icon="✅")
+
+
+
 
